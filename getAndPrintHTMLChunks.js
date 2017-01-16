80c2d7b6fa89e261a30fef48b1edd314eeeac57b
Andrew Kim <your@email.com>
2017-01-16 12:21:47 -0500

    Switched single quotes to double quotes. Changed some spacing for personal style.

--- getAndPrintHTMLChunks.js
+++ getAndPrintHTMLChunks.js
@@ -1,23 +1,22 @@
 const https = require("https");
 
-
 const getAndPrintHTMLChunks = () => {
 
   const requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    host: "sytantris.github.io",
+    path: "/http-examples/step1.html"
   };
 
   https.get(requestOptions, (response) => {
 
-    response.setEncoding('utf8');
+    response.setEncoding("utf8");
 
-    response.on('data', (data) => {
+    response.on("data", (data) => {
       console.log(`Chunk Received. Data: ${data}\n`);
     });
 
-    response.on('end', () => {
-      console.log('Response stream complete.');
+    response.on("end", () => {
+      console.log("Response stream complete.");
     });
 
   });
